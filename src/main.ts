85313b42f6fc1c232f1bed7afe80ef2eb5540485
dinushchathurya <dinushchathurya21@gmail.com>
2021-12-28 21:18:20 +0530

    config: Configure main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // await app.listen(3000);
-  const expressApp = express();
-  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp)
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
